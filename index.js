e9622040fce6d0c28c1c0a6fad219582e0b8ad5d
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2021-01-27 17:28:39 -0300

    gotta transpile

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+function curry(fn) {
+    return function () {
+        var _a;
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        return args.length >= fn.length ? fn.apply(void 0, args) : curry((_a = fn).bind.apply(_a, __spreadArrays([undefined], args)));
+    };
+}
+exports.default = curry;
