7a08fa93a86aa117208767d1a5152bc8cc373a5e
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2021-02-01 10:19:54 -0300

    Exported the types used

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
 /** Thank you mjakic#4536516, jcalz#2887218 and stack overflow
  * @src https://stackoverflow.com/questions/63903982/how-to-write-curry-and-compose-in-typescript-4
  */
-declare type Curried<A extends any[], R> = <P extends Partial<A>>(...args: P) => P extends A ? R : A extends [...SameLength<P>, ...infer S] ? S extends any[] ? Curried<S, R> : never : never;
-declare type SameLength<T extends any[]> = Extract<{
+export declare type Curried<A extends any[], R> = <P extends Partial<A>>(...args: P) => P extends A ? R : A extends [...SameLength<P>, ...infer S] ? S extends any[] ? Curried<S, R> : never : never;
+export declare type SameLength<T extends any[]> = Extract<{
     [K in keyof T]: any;
 }, any[]>;
 export default function curry<A extends any[], R>(fn: (...args: A) => R): Curried<A, R>;
-export {};
